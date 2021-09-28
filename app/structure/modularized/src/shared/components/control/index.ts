7c9bc1a1f18abe7be7534d6c4e3f4ee7ab97fc62
Ourai Lin <ourairyu@gmail.com>
2021-09-28 20:38:55 +0800

    chore: upgrade `handie-vue` and `@handie/bulbasaur`

--- app/structure/modularized/src/shared/components/control/index.ts
+++ app/structure/modularized/src/shared/components/control/index.ts
@@ -1,16 +1,4 @@
-import {
-  Icon,
-  Button,
-  TextInput,
-  TextArea,
-  Select,
-  Option,
-  Radio,
-  RadioGroup,
-  Switch,
-} from '@kokiri/view-ui';
+// export * from '@handie/bulbasaur/dist/controls';
+export * from '@kokiri/view-ui';
 
-export * from '@handie/bulbasaur/dist/controls';
-
-export { Icon, Button, TextInput, TextArea, Select, Option, Radio, RadioGroup, Switch };
-export { Input, Form, FormItem, Tooltip } from 'element-ui';
+export { Input } from 'element-ui';
